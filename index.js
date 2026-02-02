dcec8b51ff2ed4f2ff28b556874b2e986ddf2807
Walid Frakchi Sekkouri <235080725+wafrle@users.noreply.github.com>
2026-02-02 11:09:58 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,273 @@
+const imageObject = {
+  // *** France ***
+  Paris: "https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg",
+  Lyon: "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg",
+  Toulouse:
+    "https://images.pexels.com/photos/1619311/pexels-photo-1619311.jpeg",
+  Nice: "https://images.pexels.com/photos/2724677/pexels-photo-2724677.jpeg",
+  Bordeaux: "https://images.pexels.com/photos/744847/pexels-photo-744847.jpeg",
+  Marseille: "https://images.pexels.com/photos/334144/pexels-photo-334144.jpeg",
+  Montpellier:
+    "https://images.pexels.com/photos/733162/pexels-photo-733162.jpeg",
+  Strasbourg:
+    "https://images.pexels.com/photos/2445215/pexels-photo-2445215.jpeg",
+
+  // *** Spain Mainland ***
+  Madrid: "https://images.pexels.com/photos/256150/pexels-photo-256150.jpeg",
+  Barcelona: "https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg",
+  Valencia:
+    "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg",
+  Seville: "https://images.pexels.com/photos/329971/pexels-photo-329971.jpeg",
+  Bilbao: "https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg",
+  Zaragoza: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
+  Malaga: "https://images.pexels.com/photos/4173465/pexels-photo-4173465.jpeg",
+  Murcia: "https://images.pexels.com/photos/4173447/pexels-photo-4173447.jpeg",
+
+  "Santiago de Compostela":
+    "https://images.pexels.com/photos/21014/pexels-photo.jpg",
+  Toledo: "https://images.pexels.com/photos/377111/pexels-photo-377111.jpeg",
+  Alicante:
+    "https://images.pexels.com/photos/4173446/pexels-photo-4173446.jpeg",
+  Granada: "https://images.pexels.com/photos/4173478/pexels-photo-4173478.jpeg",
+  Cadiz: "https://images.pexels.com/photos/109919/pexels-photo-109919.jpeg",
+  Valladolid:
+    "https://images.pexels.com/photos/219998/pexels-photo-219998.jpeg",
+  "San Sebastián":
+    "https://images.pexels.com/photos/530164/pexels-photo-530164.jpeg",
+  Salamanca:
+    "https://images.pexels.com/photos/2343070/pexels-photo-2343070.jpeg",
+
+  Santander: "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg",
+  Vigo: "https://images.pexels.com/photos/45844/pexels-photo-45844.jpeg",
+  "Palma (Mallorca)":
+    "https://images.pexels.com/photos/115478/pexels-photo-115478.jpeg",
+  "Vitoria-Gasteiz":
+    "https://images.pexels.com/photos/373902/pexels-photo-373902.jpeg",
+  "Granada Alcazaba":
+    "https://images.pexels.com/photos/4173481/pexels-photo-4173481.jpeg",
+  Cordoba: "https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg",
+  "Zaragoza Basillica":
+    "https://images.pexels.com/photos/461197/pexels-photo-461197.jpeg",
+  "Toledo Alcazar":
+    "https://images.pexels.com/photos/377110/pexels-photo-377110.jpeg",
+
+  // *** Portugal ***
+  Lisbon: "https://images.pexels.com/photos/164634/pexels-photo-164634.jpeg",
+  Porto: "https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg",
+  Braga: "https://images.pexels.com/photos/4173449/pexels-photo-4173449.jpeg",
+  Coimbra: "https://images.pexels.com/photos/3796283/pexels-photo-3796283.jpeg",
+  Faro: "https://images.pexels.com/photos/4173483/pexels-photo-4173483.jpeg",
+  Setúbal: "https://images.pexels.com/photos/460622/pexels-photo-460622.jpeg",
+  Guimarães:
+    "https://images.pexels.com/photos/4173494/pexels-photo-4173494.jpeg",
+  Évora: "https://images.pexels.com/photos/4173492/pexels-photo-4173492.jpeg",
+
+  // *** Canary Islands (Spain) ***
+  "Santa Cruz de Tenerife":
+    "https://images.pexels.com/photos/4592254/pexels-photo-4592254.jpeg",
+  "Las Palmas de Gran Canaria":
+    "https://images.pexels.com/photos/34248790/pexels-photo-34248790.jpeg",
+  "San Cristóbal de La Laguna":
+    "https://images.pexels.com/photos/4313638/pexels-photo-4313638.jpeg",
+  "Puerto de la Cruz":
+    "https://images.pexels.com/photos/4173493/pexels-photo-4173493.jpeg",
+  "Costa Adeje":
+    "https://images.pexels.com/photos/4173500/pexels-photo-4173500.jpeg",
+  "Playa de Las Americas":
+    "https://images.pexels.com/photos/4173501/pexels-photo-4173501.jpeg",
+  "Arrecife (Lanzarote)":
+    "https://images.pexels.com/photos/4173502/pexels-photo-4173502.jpeg",
+  "Corralejo (Fuerteventura)":
+    "https://images.pexels.com/photos/4173503/pexels-photo-4173503.jpeg",
+
+  // *** Balearic Islands (Spain) ***
+  Mallorca: "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg",
+  "Ibiza Town":
+    "https://images.pexels.com/photos/3601420/pexels-photo-3601420.jpeg",
+  "Mahón (Menorca)":
+    "https://images.pexels.com/photos/4173504/pexels-photo-4173504.jpeg",
+  "Pollensa (Mallorca)":
+    "https://images.pexels.com/photos/4173505/pexels-photo-4173505.jpeg",
+  "Palma Cathedral":
+    "https://images.pexels.com/photos/115473/pexels-photo-115473.jpeg",
+  "Port de Sóller":
+    "https://images.pexels.com/photos/4173506/pexels-photo-4173506.jpeg",
+  "Ciutadella (Menorca)":
+    "https://images.pexels.com/photos/4173507/pexels-photo-4173507.jpeg",
+  "Eivissa Old Town":
+    "https://images.pexels.com/photos/3601421/pexels-photo-3601421.jpeg",
+
+  // *** Microstates & Territories ***
+  "Andorra la Vella":
+    "https://images.pexels.com/photos/3677814/pexels-photo-3677814.jpeg",
+  Gibraltar:
+    "https://images.pexels.com/photos/4752959/pexels-photo-4752959.jpeg",
+};
+
+Object.values(imageObject).forEach(imageUrl => {
+  const image = new Image();
+
+  image.src = imageUrl;
+});
+
+const locationMap = [
+  [
+    "Braga",
+    "Porto",
+    "Bilbao",
+    "San Sebastián",
+    "Bordeaux",
+    "Paris",
+    "Lyon",
+    "Strasbourg",
+  ],
+  [
+    "Guimarães",
+    "Coimbra",
+    "Vigo",
+    "Santander",
+    "Toulouse",
+    "Montpellier",
+    "Nice",
+    "Marseille",
+  ],
+  [
+    "Lisbon",
+    "Évora",
+    "Setúbal",
+    "Valladolid",
+    "Madrid",
+    "Barcelona",
+    "Valencia",
+    "Palma (Mallorca)",
+  ],
+  [
+    "Faro",
+    "Seville",
+    "Cordoba",
+    "Toledo",
+    "Zaragoza",
+    "Alicante",
+    "Murcia",
+    "Granada",
+  ],
+  [
+    "Santa Cruz de Tenerife",
+    "Las Palmas de Gran Canaria",
+    "San Cristóbal de La Laguna",
+    "Puerto de la Cruz",
+    "Costa Adeje",
+    "Playa de Las Americas",
+    "Arrecife (Lanzarote)",
+    "Corralejo (Fuerteventura)",
+  ],
+  [
+    "Mallorca",
+    "Ibiza Town",
+    "Mahón (Menorca)",
+    "Pollensa (Mallorca)",
+    "Palma Cathedral",
+    "Port de Sóller",
+    "Ciutadella (Menorca)",
+    "Eivissa Old Town",
+  ],
+  [
+    "San Sebastián",
+    "Vitoria-Gasteiz",
+    "Zaragoza Basillica",
+    "Barcelona",
+    "Toledo Alcazar",
+    "Cadiz",
+    "Malaga",
+    "Murcia",
+  ],
+  [
+    "Andorra la Vella",
+    "Gibraltar",
+    "Lisbon",
+    "Madrid",
+    "Paris",
+    "Nice",
+    "Toulouse",
+    "Bordeaux",
+  ],
+];
+
+let [locationX, locationY] = /* Madrid */[2, 3];
+
+for (let axisXIndex = 0; axisXIndex < locationMap.length; ++axisXIndex) {
+  let axisArray = locationMap[axisXIndex];
+
+  for (let axisYIndex = 0; axisYIndex < axisArray.length; ++axisYIndex)
+    console.log(axisArray[axisYIndex], imageObject[axisArray[axisYIndex]]);
+}
+
+const buttonNorth = document.getElementById("button-north");
+const buttonWest = document.getElementById("button-west");
+const buttonSouth = document.getElementById("button-south");
+const buttonEast = document.getElementById("button-east");
+
+const imageViewport = document.getElementById("image-viewport");
+const locationLabel = document.getElementById("location-label");
+
+{
+  const locationName = locationMap[locationX][locationY];
+
+  locationLabel.textContent = locationName;
+
+  imageViewport.setAttribute("src", imageObject[locationName]);
+}
+
+function createHandler(targetDirection) {
+  return function moveHandler(eventObject) {
+    const { target: targetButton } = eventObject;
+
+    console.log(targetButton, targetDirection);
+
+    let sign;
+
+    switch (targetDirection) {
+      case "button-north":
+      case "button-south":
+        sign = targetDirection === "button-south" ? -1 : 1;
+
+        locationY = (locationY + sign) % locationMap[locationX].length;
+
+        break;
+      case "button-west":
+      case "button-east":
+        sign = targetDirection === "button-west" ? -1 : 1;
+
+        locationX = (locationX + sign) % locationMap.length;
+
+        break;
+
+      default:
+        console.error(`error: unknown move target '${targetDirection}'`);
+
+        return;
+    }
+
+    const locationName = locationMap[locationX][locationY];
+
+    locationLabel.textContent = locationName;
+
+    imageViewport.setAttribute("src", imageObject[locationName]);
+  }
+}
+
+{
+  function handleButton(targetButton) {
+    const { id: targetId } = targetButton;
+
+    return targetButton.addEventListener("click", createHandler(targetId));
+  }
+
+  (
+    handleButton(buttonNorth),
+    handleButton(buttonSouth),
+    handleButton(buttonWest),
+    handleButton(buttonEast)
+  );
+};
+
